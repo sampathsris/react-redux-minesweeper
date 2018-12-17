53192081d8011d0d0e745318596410d3dded48a9
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-12-17 11:15:15 +0530

    Updates

---
+++ precache-manifest.dbf191f891e4e04cc08c23416efaa87c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1322f3dd3107071ab95",
+    "url": "/react-redux-minesweeper/static/js/runtime~main.c1322f3d.js"
+  },
+  {
+    "revision": "8d2d7b3e98dce4d46f29",
+    "url": "/react-redux-minesweeper/static/js/main.8d2d7b3e.chunk.js"
+  },
+  {
+    "revision": "d4164a5ca530038ac311",
+    "url": "/react-redux-minesweeper/static/js/1.d4164a5c.chunk.js"
+  },
+  {
+    "revision": "8d2d7b3e98dce4d46f29",
+    "url": "/react-redux-minesweeper/static/css/main.66661ff8.chunk.css"
+  },
+  {
+    "revision": "7a27828fc2bc80a3e994128341ff3f52",
+    "url": "/react-redux-minesweeper/index.html"
+  }
+];
